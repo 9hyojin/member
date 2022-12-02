a2dd08f4c89757c53656e67fd2fa608dbb56eedd
9hyojin <koohyojin1205@gmail.com>
2022-12-02 17:22:57 +0900

    connect with db

--- src/main/java/com/koo/member/MemberApplication.java
+++ src/main/java/com/koo/member/MemberApplication.java
@@ -1,14 +1,17 @@
 package com.koo.member;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MemberApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(MemberApplication.class, args);
+
     }
 
 }
